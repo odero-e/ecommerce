96de184e1b9ab154bacbbaf285977550e20479de
odero-e <142809816+odero-e@users.noreply.github.com>
2023-08-22 15:06:03 +0300

    images

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
       <View style={styles.carContainer}>
+
+        <ImageBackground 
+          source={require('./assets/images/ModelX.jpeg')}
+          style={styles.image}
+        />
 
         <View style={styles.titles}>
           
@@ -38,13 +43,26 @@ const styles = StyleSheet.create({
 
   titles: {
     marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
 
   },
 
   title: {
+    fontSize: 40,
+    fontWeight: '500',
 
   },
   subtitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+
+  },
+
+  image: {
+
+    width: '100%',
+    height: '100%',
 
   }
 
